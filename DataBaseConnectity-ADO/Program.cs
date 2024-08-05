b73d35819ca09f9a685f5c91931923d8d82f86eb
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-05 12:07:24 +0530

    Add project files.

---
+++ DataBaseConnectity-ADO/Program.cs
@@ -0,0 +1,62 @@
+﻿// 1st step install package System.Data.SqlClient
+
+// 2nd step
+using System.Data.SqlClient;
+namespace DataBaseConnectity_ADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3 step
+
+            string connectionString = "server=ANAMIKA\\SQLSERVER;database=wiproDb;" +
+                "integrated security=true";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            // step 4
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "select * from employee";
+            command.Connection = connection;
+
+            // step 5
+
+            connection.Open();  // this will establish between console app & database
+
+            // step 4
+            // run the query
+            SqlDataReader reader =  command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader[0] + " " + reader[1] + " " + reader[2]);
+                }
+            }
+            else
+            {
+
+                Console.WriteLine("there are no records");
+            }
+            reader.Close();
+            connection.Close();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // }
+        }
+}
