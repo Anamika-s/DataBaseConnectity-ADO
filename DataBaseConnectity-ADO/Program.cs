ff7e35d765786f49c0d66406c6c18a19ae421132
Anamika Sawhney <anamika.sawhney22@gmail.com>
2024-08-05 13:24:14 +0530

    added other operations

--- DataBaseConnectity-ADO/Program.cs
+++ DataBaseConnectity-ADO/Program.cs
@@ -6,16 +6,48 @@ namespace DataBaseConnectity_ADO
 {
     internal class Program
     {
+        static SqlConnection connection;
+        static SqlCommand command;
+
         static void Main(string[] args)
         {
-            // 3 step
-
+            int ch = Menu();
+            switch(ch)
+            {
+                case 1: GetRecords(); break;
+                case 2:  AddRecord(); break;
+                case 3:  DeleteRecord(); break;
+                case 4: EditRecord(); break;
+                default:
+                    Console.WriteLine("invalid choice"); break;
+            }
+        }
+        static SqlConnection GetConnection()
+        {
             string connectionString = "server=ANAMIKA\\SQLSERVER;database=wiproDb;" +
-                "integrated security=true";
-            SqlConnection connection = new SqlConnection(connectionString);
+              "integrated security=true";
+            connection = new SqlConnection(connectionString);
+            return connection;
+        }
 
+        static int Menu()
+        {
+            Console.WriteLine("1. Get Records");
+            Console.WriteLine("2. Insert Record");
+            Console.WriteLine("3. Delete Record");
+            Console.WriteLine("4. Edit Record");
+            Console.WriteLine("enter your choice");
+            int ch = byte.Parse(Console.ReadLine());
+            return ch;
+        }
+
+        static void GetRecords()
+        {
             // step 4
-            SqlCommand command = new SqlCommand();
+            string connectionString = "server=ANAMIKA\\SQLSERVER;database=wiproDb;" +
+              "integrated security=true";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
             command.CommandText = "select * from employee";
             command.Connection = connection;
 
@@ -25,7 +57,7 @@ namespace DataBaseConnectity_ADO
 
             // step 4
             // run the query
-            SqlDataReader reader =  command.ExecuteReader();
+            SqlDataReader reader = command.ExecuteReader();
             if (reader.HasRows)
             {
                 while (reader.Read())
@@ -40,23 +72,65 @@ namespace DataBaseConnectity_ADO
             }
             reader.Close();
             connection.Close();
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            // }
         }
+        static void AddRecord()
+        {
+            string connectionString = "server=ANAMIKA\\SQLSERVER;database=wiproDb;" +
+              "integrated security=true";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+            command.CommandText = "insert into employee values (2,'Deepak','HR',9000)";
+            command.Connection = connection;
+            connection.Open();
+            command.ExecuteNonQuery(); // for insert update delete
+            connection.Close();
+
+        }
+
+
+        static void DeleteRecord()
+        {
+            string connectionString = "server=ANAMIKA\\SQLSERVER;database=wiproDb;" +
+              "integrated security=true";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+            command.CommandText = "delete from employee where id=1";
+            command.Connection = connection;
+            connection.Open();
+            command.ExecuteNonQuery(); // for insert update delete
+            connection.Close();
+
+        }
+
+        static void EditRecord()
+        {
+            string connectionString = "server=ANAMIKA\\SQLSERVER;database=wiproDb;" +
+              "integrated security=true";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+            command.CommandText = "update employee set salary=50000 where id=1";
+            command.Connection = connection;
+            connection.Open();
+            command.ExecuteNonQuery(); // for insert update delete
+            connection.Close();
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // }
 }
+ 
